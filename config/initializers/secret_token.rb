44740d9daf2e1536231adffd91cad364b2214177
Ryan Watson <ryan@accrew.co>
2013-07-20 19:50:28 -0400

    Created Accrewbook

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Accrewbook::Application.config.secret_token = '390151ed7f8d4bc5756b722b8c56f45edace52557270cd40d43777fff782bc46ca31c1ebdfab6c5eccf3fb03f4b6a94a36e9dc74cfe411eaea15f8eddc08cb27'
